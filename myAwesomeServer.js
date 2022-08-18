eb8b83af04693a3104eecc5979378aa9b837bbd4
Gil BMP <94876642+gilbmporto@users.noreply.github.com>
2022-08-18 19:48:04 -0300

    2nd Commit - Server is Now Functioning (CR Only)
    Server is now functioning for GET and POST requests, but anyway, it's fully functional for the front-end intended to be used with it. Discover the facts about a group of friends called Surf's App and enjoy it! 😎👌

--- myAwesomeServer.js
+++ myAwesomeServer.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const surfsAppData = require('./surfsAppData');
+const surfsAppDatabase = require('./surfsAppData');
 
 const PORT = process.env.PORT || 5001;
 
@@ -40,6 +41,12 @@ app.get('/surfs-app/random', (req, res, next) => {
     });
 })
 
+app.get('/surfs-app/all', (req, res, next) => {
+    res.status(200).send({
+        data: surfsAppData
+    })
+})
+
 app.get(`/surfs-app/:person`, (req, res, next) => {
     let personFName = (req.query.personName).toLowerCase();
     console.log(req.query.firstName);
@@ -52,6 +59,51 @@ app.get(`/surfs-app/:person`, (req, res, next) => {
         };
     }
     res.status(404).send('Name not found');
+});
+
+app.get('/surfs-app/post-new-fact/:newfact', (req, res, next) => {
+    let userName = req.query.username;
+    let passWord = req.query.password;
+    let authAccess = false;
+    const validUsers = [
+        {
+            username: 'gil.porto',
+            password: '231546'
+        }, 
+        {
+            username: 'davi.navarro',
+            password: '786989'    
+        }];
+    for (const user of validUsers) {
+        if (user.username === userName && user.password === passWord) {
+            authAccess = true;
+            res.status(200).send({auth: authAccess, data: surfsAppData})
+            return user;
+        } else {
+            res.status(403).send({auth: authAccess});
+            return user;
+            };
+        }
+});
+
+app.post('/surfs-app/post-new-fact/:newfact',(req, res, next) => {
+    let personToAddFact = req.query.personToAddFact;
+    let newFactToBeAdded = req.query.factToBePosted;
+    for (obj of surfsAppDatabase) {
+        if (personToAddFact === obj.firstName) {
+            (obj.facts).push({
+                factId: ((obj.facts).length + 1),
+                info: newFactToBeAdded
+            });
+            res.send({
+                newFactId: ((obj.facts).length + 1),
+                personName: personToAddFact,
+                newFact: newFactToBeAdded
+            });
+        } else {
+            res.status(404).send('Person not found!');
+        };
+    };
 });
 
 app.listen(PORT, () => {
