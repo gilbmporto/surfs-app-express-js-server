230737c33008bcb966e078932733e8692f97114f
Gil BMP <94876642+gilbmporto@users.noreply.github.com>
2022-08-17 21:41:32 -0300

    2nd Commit - Search Facts By Name Now Working!
    Read the title.

--- myAwesomeServer.js
+++ myAwesomeServer.js
@@ -6,12 +6,25 @@ const surfsAppData = require('./surfsAppData');
 
 const PORT = process.env.PORT || 5001;
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
-  });
+});
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
 
 app.use(morgan('dev'));
 
@@ -26,6 +39,20 @@ app.get('/surfs-app/random', (req, res, next) => {
         data: randomData
     });
 })
+
+app.get(`/surfs-app/:person`, (req, res, next) => {
+    let personFName = (req.query.personName).toLowerCase();
+    console.log(req.query.firstName);
+    for (obj of surfsAppData) {
+        if ((obj.firstName).toLowerCase() == personFName) {
+            res.send({
+                data: obj
+            })
+            return obj;
+        };
+    }
+    res.status(404).send('Name not found');
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
